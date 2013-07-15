813272c46e8598798911dfdd8f459d48ae3e3b70
nicolasenno <nicola.senno@redturtle.it>
2013-07-15 17:04:14 +0000

    aggiunto test unicode al metodo replace marker

--- pd/contentrules/sms/actions/sms.py
+++ pd/contentrules/sms/actions/sms.py
@@ -103,7 +103,9 @@ class SMSActionExecutor(object):
         '''Replace markers in text with the values in the mapping
         '''
         for key, value in self.mapping.iteritems():
-            text = text.replace('${%s}' % key, value.decode('utf-8'))
+            if not isinstance(value, unicode):
+                value = value.decode('utf-8')
+            text = text.replace('${%s}' % key, value)
         return text
 
     def get_from(self):
